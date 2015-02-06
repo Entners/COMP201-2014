6e89fac37a5de66a2a76fd7c13dda85329284b2f
Samuel Entner <samuelentner@wit.edu>
2015-02-06 13:26:37 -0500

    Commiting

---
+++ lab2/lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab2
+//
+//  Created by Samuel Entner on 2/5/15.
+//  Copyright (c) 2015 Samuel Entner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
