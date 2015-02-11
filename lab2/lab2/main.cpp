0753b5ee7e2ef3f060394e5c33171b2479187d5a
Samuel Entner <samuelentner@wit.edu>
2015-02-11 13:20:39 -0500

    Lab2 Done

--- lab2/lab2/main.cpp
+++ lab2/lab2/main.cpp
@@ -1,15 +1,54 @@
-//
-//  main.cpp
-//  lab2
-//
-//  Created by Samuel Entner on 2/5/15.
-//  Copyright (c) 2015 Samuel Entner. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string token;
+    vector<int> stack;
+    int left, right;
+    cout << "Enter a postfix expression: ";
+    while (cin >> token) {
+        if (token == "+") {
+            right = stack.back();
+            stack.pop_back();
+            left = stack.back();
+            stack.pop_back();
+            stack.push_back(left + right);
+        } else if (token == "-") {
+            right = stack.back();
+            stack.pop_back();
+            left = stack.back();
+            stack.pop_back();
+            stack.push_back(left - right);
+        } else if (token == "/") {
+            right = stack.back();
+            stack.pop_back();
+            left = stack.back();
+            stack.pop_back();
+            stack.push_back(left / right);
+        } else if (token == "%") {
+            right = stack.back();
+            stack.pop_back();
+            left = stack.back();
+            stack.pop_back();
+            stack.push_back(left % right);
+        } else if (token == "*") {
+            right = stack.back();
+            stack.pop_back();
+            left = stack.back();
+            stack.pop_back();
+            stack.push_back(left * right);
+        } else if (token == ".") {
+            break;
+        } else {
+            stack.push_back(stoi(token));
+        }
+    }
+    for (int i = 0; i < stack.size(); i++) {
+        cout << stack[i] << endl;
+    }
     return 0;
 }
